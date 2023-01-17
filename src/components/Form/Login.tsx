84581405f503f6938481bb4d6ec2f09b7b4cdca5
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-01-17 10:04:33 -0400

    login page

--- src/components/Form/Login.tsx
+++ src/components/Form/Login.tsx
@@ -3,30 +3,37 @@ import InputForm from "./Input";
 
 import styles from "./styles/Login.module.css";
 import stylesInp from "./styles/Input.module.css";
+import stylesBtn from "./styles/Btn.module.css";
 
 const Login = () => (
   <div className={styles.container}>
     <h1>Faça seu Login</h1>
-    <label htmlFor="emailInput">Login</label>
-    <InputForm
-      place="Digite seu email"
-      classType={stylesInp.input}
-      name="emailInput"
-      inputType="email"
-    />
 
-    <label htmlFor="passwordInput">Senha</label>
-    <InputForm
-      place="Digite sua senha"
-      classType={stylesInp.input}
-      name="emailInput"
-      inputType="password"
-    />
+    <div className={styles.inputSection}>
+      <label htmlFor="emailInput">Login</label>
+      <InputForm
+        place="Digite seu email"
+        classType={stylesInp.input}
+        name="emailInput"
+        inputType="email"
+      />
+    </div>
 
+    <div className={styles.inputSection}>
+      <label htmlFor="passwordInput">Senha</label>
+      <InputForm
+        place="Digite sua senha"
+        classType={stylesInp.input}
+        name="emailInput"
+        inputType="password"
+      />
+    </div>
     <ButtonForm
       value="Enviar"
-      classType="login"
+      classType={stylesBtn.btn}
     />
+
+    <p> Não possui cadastro? <a href="#">Clique aqui.</a></p>
   </div>
 );
 
