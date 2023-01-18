cca7ae907add2cb594b758279fbad7f8e32179d0
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-01-18 09:24:38 -0400

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import Login from "./components/Form/Login";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import styles from "./App.module.css";
+
+import Login from "./components/Form/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className={styles.container}>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home /> }/>
+          <Route path="/login" element={<Login /> }/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,8 @@
+const Home = () => (
+  <>
+    <h1>estamos na home!</h1>
+    <a href="/login">ir para login</a>
+  </>
+);
+
+export default Home;
