750021320ab273b6950d5061a0201f02bbf89693
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-01-23 11:42:54 -0400

    update header

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -5,9 +5,16 @@ import { Space } from 'antd';
 
 import style from "./styles/Header.module.css";
 
-const items1: MenuProps['items'] = ['1', '2', '3', '4', '5'].map((key) => ({
+const date = new Date();
+const months = [
+  'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+  'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+];
+
+
+const items1: MenuProps['items'] = months.map((key) => ({
   key,
-  label: `dia ${key}`,
+  label: `${key}`,
 }));
 
 const HeaderHome = () => {
@@ -18,7 +25,7 @@ const HeaderHome = () => {
       <Space className={style.iconLogo}>
         <CarryOutOutlined style={{color: 'white', fontSize: '55px'}}/>
       </Space>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} items={items1} className={style.logoItens}/>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[`${months[date.getMonth()]}`]} items={items1} className={style.logoItens}/>
     </Header>
   )
 }
