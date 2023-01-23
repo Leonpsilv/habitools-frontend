e79b043b52d17711958f4d74cb7e2e2acb354cc1
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-01-23 10:00:33 -0400

    home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,63 @@
-const Home = () => (
-  <>
-    <h1>estamos na home!</h1>
-    <a href="/login">ir para login</a>
-  </>
+import React from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined, CarryOutOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+import HeaderHome from './Header';
+
+const { Content, Footer, Sider } = Layout;
+
+const items2: MenuProps['items'] = [CarryOutOutlined, LaptopOutlined, NotificationOutlined, UserOutlined, LaptopOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
 );
+
+const Home = () => {
+    const {
+      token: { colorBgContainer },
+    } = theme.useToken();
+  
+    return (
+      <Layout>
+        <HeaderHome />
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Layout style={{ padding: '24px 0', background: colorBgContainer }}>
+            <Sider style={{ background: colorBgContainer }} width={200}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%' }}
+                items={items2}
+              />
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    );
+
+};
 
 export default Home;
