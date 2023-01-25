19b20cf99962424d1537033e98cb08623c989b64
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-01-25 11:22:59 -0400

    getAllDaysInMonth

--- src/Hooks/useGetDate.ts
+++ src/Hooks/useGetDate.ts
@@ -4,12 +4,32 @@ const useGetDate = () => {
     'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
     'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
   ];
-  const dayWeek = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira'];
+  const dayWeek = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
+
+  function getAllDaysInMonth(year: number, month: number) {
+    const date = new Date(year, month, 1);
+    const dates = [];
+  
+    while (date.getMonth() === month) {
+      const [dayWeek, month, day] = new Date(date).toString().split(" ");
+      const infos = {
+        month,
+        day,
+        dayWeek
+      }
+
+      dates.push(infos);
+      date.setDate(date.getDate() + 1);
+    }
+  
+    return dates;
+  }
 
   return {
     date,
     months,
-    dayWeek
+    dayWeek,
+    getAllDaysInMonth
   }
 }
 
